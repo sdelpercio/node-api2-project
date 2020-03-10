9537bcdcd1cbb97411d498c89555572ee28e7193
Shawn DelPercio <sdelperc@gmail.com>
2020-03-10 16:11:52 -0400

    added get route for single blog posts

--- blog-posts/blog-posts-router.js
+++ blog-posts/blog-posts-router.js
@@ -3,6 +3,7 @@ const db = require('../data/db');
 
 const router = express.Router();
 
+// GET all blog posts
 router.get('/', (req, res) => {
 	db.find()
 		.then(posts => {
@@ -12,6 +13,26 @@ router.get('/', (req, res) => {
 			res
 				.status(500)
 				.json({ error: 'The posts information could not be retrieved.' });
+		});
+});
+
+// GET a single blog post
+router.get('/:id', (req, res) => {
+	db.findById(req.params.id)
+		.then(post => {
+			if (post) {
+				res.status(200).json(post);
+			} else {
+				res
+					.status(404)
+					.json({ error: 'The post with the specified ID does not exist.' });
+			}
+		})
+		.catch(error => {
+			console.log(error);
+			res
+				.status(500)
+				.json({ error: 'The post information could not be retrieved.' });
 		});
 });
 
